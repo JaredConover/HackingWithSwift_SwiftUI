1fca4725633ec1d7c2f6ca6583ce7eed41ec935d
Jared Conover <jconover@vendere.ca>
2021-08-12 11:06:26 -0400

    First experiments

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        // Navigation view stops elements from scrolling behind system ui (clock, etc)
+        NavigationView {
+            
+            Form {
+                
+                // We cannot add more than 10 elements to each group
+                Group {
+                    Text("Hello, world!").padding()
+                    Text("Hello, world!").padding()
+                    Text("Hello, world!").padding()
+                }
+                
+                // A section behaves like a group but with added visual seperation
+                Section {
+                    Text("Goodbye, world!").padding()
+                    Text("Goodbye, world!").padding()
+                    Text("Goodbye, world!").padding()
+                    Text("Goodbye, world!").padding()
+                    Text("Goodbye, world!").padding()
+                }
+            }
+            
+            // Setting nav title inline puts title inside nav bar 
+            .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
