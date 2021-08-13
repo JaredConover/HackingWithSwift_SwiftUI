513a5129fe187bc5e625ea028d1f30ab66221a09
Jared Conover <jconover@vendere.ca>
2021-08-13 16:19:55 -0400

    01 - WeSplit - Beginning of wesplit, added check amount input and number of people picker in form with nav view

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount).padding()
+                        .keyboardType(.decimalPad)
+                    
+                    // Inside of a form the picker needs to be nested in a navigation view
+                    // or SwiftUI wont be able to create the screen of options
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
+                    }
+                    .padding()
+                }
+                
+                Section {
+                    Text("$\(checkAmount)").padding()
+                }
+            }
+            // This title is associated to the form instead of the navigation view because the navigation view may need to display titles for may different views
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone SE (2nd generation)")
+            .previewLayout(.device)
+            .preferredColorScheme(.dark)
     }
 }
