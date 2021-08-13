46e2e223d2054043741aad5549185171f73d210e
Jared Conover <jconover@vendere.ca>
2021-08-13 12:29:49 -0400

    Add tapcount property wrapper for use with button

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // A property wrapper is used so we can modify the struct
+    // @State is a property wrapper recommended for simple properties that exist
+    // in only one view so we add the private access modifier
+    @State private var tapCount = 0
+    
     var body: some View {
         
         // Navigation view stops elements from scrolling behind system ui (clock, etc)
         NavigationView {
             
             Form {
+                
+                Section {
+                    Button("Tap Count: \(tapCount)") {
+                        self.tapCount += 1
+                    }
+                }
                 
                 // We cannot add more than 10 elements to each group
                 Group {
@@ -27,12 +39,10 @@ struct ContentView: View {
                     Text("Goodbye, world!").padding()
                     Text("Goodbye, world!").padding()
                     Text("Goodbye, world!").padding()
-                    Text("Goodbye, world!").padding()
-                    Text("Goodbye, world!").padding()
                 }
             }
             
-            // Setting nav title inline puts title inside nav bar 
+            // Setting nav title inline puts title inside nav bar
             .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
         }
     }
